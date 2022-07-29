9dc13902f454ea333ba083da347926f8bd569fff
JIHOON4680 <jihoon4680@gmail.com>
2022-07-29 20:43:43 +0900

    시간제약 추가, 오토레이아웃, 데이터베이스 업데이트, 토스트메세지, 에러해결

--- Habits/CheckVC.swift
+++ Habits/CheckVC.swift
@@ -4,16 +4,22 @@
 //
 //  Created by 안지훈 on 7/23/22.
 
+// Model CreatedTime -> 이름변경 요망.
+// ViewController 파일 여러개 만들고 그룹화
+// Beta때는 Realm으로 하는게 좋을 듯 ?? ImageVIew LayOut을 snapkit으로
+
 
 import UIKit
 import RealmSwift
+import Toast_Swift
 
 class CheckVC: UIViewController {
     
     @IBOutlet weak var checkOne: UIButton! // 첫번째 버튼
     @IBOutlet weak var checkTwo: UIButton! // 두번쨰 버튼
     @IBOutlet weak var checkThree: UIButton! // 세번째 버튼
-  
+    @IBOutlet weak var successButton: UIButton!
+    
     
     @IBOutlet weak var checkVCTitle: UILabel!{
         didSet{
@@ -26,15 +32,31 @@ class CheckVC: UIViewController {
     // createTime -> pastTime으로 넘겨서 현재시간이랑 비교를 해야겠네
     // Date
     var initCheckVCTitle : String = "" // chekTitle 바꿀 데이터 전달 받을 변수
-    var currentTime : String = "" // 그냥 처음에
-    var pastTime : Date?
+    var clickedTime : Date = Date()
+    
+    var dateStr : Date = {
+        let date = "2022-06-30 16:30"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        guard let dataResult = dateFormatter.date(from: date) else { return Date() }
+        return dataResult
+    }()
     var dayCount : Int = Int()
-
-    //MARK: - overrideMethod
+    
+    //MARK: - overrideMethod viewLifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
         getRealmData()
+        setButton()
+        setButtonImage(self.dayCount)
+ 
+
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        setRealmDate()
     }
     
     // MARK: - makeAlert (  알람메세지 )
@@ -43,13 +65,23 @@ class CheckVC: UIViewController {
         let completeAlert = UIAlertController(title: "습관 완료", message: "\(count + 1)일째 완료하셨습니다.", preferredStyle: .alert) // 완료 alert
         // 확인이 눌려야 실행
         let completeAlertAction = UIAlertAction(title: "완료", style: .default){
-            (action) in self.changButtonImage(count)
+            (action) in
+            self.changeButtonImage(count)
+            self.dayCount += 1
+            self.resetSuccessButton()
+            
         }
-        // 습관을 완료하지 못했을 때
-        let completeAlertCancel = UIAlertAction(title: "취소", style: .destructive,handler: nil)
+        // 습관을 완료하지 못했을 때  ***** 변경속도 조정필요
+        let completeAlertCancel = UIAlertAction(title: "취소", style: .destructive,handler:nil)
+        
         
         let finishAlert = UIAlertController(title: "  성공  ", message: "\(count + 1)일 달성 완료", preferredStyle: .alert)
-        let finishAlertAction = UIAlertAction(title: "확인.", style: .default)
+        let finishAlertAction = UIAlertAction(title: "확인.", style: .default){
+            _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.navigationController?.popToRootViewController(animated: true)
+            }  // 1 초뒤 팝뷰 , 메인쓰레드에서만 동장해야됨.
+        }
         
         // 알림창 설정
         completeAlert.addAction(completeAlertCancel)
@@ -59,26 +91,22 @@ class CheckVC: UIViewController {
         switch count {
         case 0, 1:
             self.present(completeAlert, animated: true, completion: nil)
-            print(count)
-        
+            //            print(count)
+            
         case 2:
-            changButtonImage(count)
+            changeButtonImage(count)
+            self.dayCount += 1
             present(finishAlert,animated: true, completion: nil)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                self.navigationController?.popToRootViewController(animated: true)
-            }  // 1 초뒤 팝뷰 , 메인쓰레드에서만 동장해야됨.-> 공부필요
         default:
             return
         }
     }
     
-  
     
     
-    
-    // MARK : - changeButtonImage (Button이미지 변경)
-    func changButtonImage(_ dayCount : Int){
-        
+    // MARK:  changeButtonImage (Button이미지 변경)
+    func changeButtonImage(_ dayCount : Int){
+        print( "count \(dayCount)")
         switch dayCount{
         case 0:
             checkOne.setImage(UIImage(systemName: "checkmark.seal.fill"), for: .normal) //첫번째아이콘
@@ -89,63 +117,142 @@ class CheckVC: UIViewController {
         default:
             return
         }
-        self.dayCount += 1
     }
-    // MARK: - IBAction Method
     
-    // MARK: clickSuccessButton ( 성공버튼 클릭 액션 )
+    
+    // MARK: - @IBAction Method
+    //MARK: clickSuccessButton ( 성공버튼 클릭 액션 )
+    
     @IBAction func clickSuccessButton(_ sender: UIButton) {
-        makeAlert(dayCount) // 완료했을 때 취소 했을 때 나눔
+        resetSuccessButton()
+        print("cleckbutton \(dayCount)")
+        print("pastTime \(clickedTime)")
+        print(Date())
+        // 버튼입력일자가 하루 지났을 떄.
+        if (compareDate(clickedTime) || self.dayCount == 0){
+            makeAlert(dayCount) // 완료했을 때 취소 했을 때 나눔
+            self.clickedTime = Date() // 버튼 누른 시간을 기억
+        } else {
+            var toastStyle = ToastStyle()
+            toastStyle.titleFont = .boldSystemFont(ofSize: 20)
+            toastStyle.titleAlignment = .center
+            toastStyle.messageAlignment = .center
+            self.view.makeToast(" 아직 하루가 지나지 않았습니다. ", duration: 1.0, position: .center, title: " ❌ 실패 ❌", image: nil, style: toastStyle, completion: nil)
+            
+        }
     }
     
 }
 
 
-// MARK: - setData Method
-
-
+// MARK: - setData Method ( ConfigVC prepare()로 받아온 데이터 )
 extension CheckVC {
     
     // MARK: configVC - prepared() 에서 데이터 전달 받는 데이터 변경 / title이 키 값
     func receiveItem(_ title : String) {
-        print("CheckVC - receiveItem called() item : \(title)")
+        //        print("CheckVC - receiveItem called() item : \(title)")
         self.initCheckVCTitle = title
     }
     
+}
+//MARK: - RealmData 처리
+extension CheckVC{
+    //MARK: getRealmData() cell에 해당하는 realm데이터 받아옴
+    func getRealmData() {
+        //        print(" CheckVC setRealmData called() ")
+        let realm = try! Realm()
+        
+        guard let data = realm.objects(Habits.self).filter(NSPredicate(format: "title = %@", initCheckVCTitle )).first else { return }
+        guard let time = data.createTime else { return } // 옵셔널 바인딩
+        let count = data.dayCount //
+        self.dayCount = count // 성공횟수
+        self.clickedTime = time
+    }
     
     
-    //MARK: - CheckVCTitle 설정
+    //MARK: setRealmData() -> 뷰 나갈떄 Realm데이터 세팅
+    func setRealmDate(){
+        //        print("CheckVC - setRealmData called() ")
+        let realm = try! Realm()
+        
+        if let data = realm.objects(Habits.self).filter(NSPredicate(format: "title = %@", initCheckVCTitle )).first{
+            try! realm.write{
+                data.createTime = self.clickedTime
+                data.dayCount = self.dayCount
+                
+            }
+        }
+        
+    }
+}
+
+//MARK: - 시간 처리
+extension CheckVC {
+    func compareDate(_ date : Date) -> Bool {
+        let currentTime = Calendar.current.dateComponents([.year , .month, .day], from: Date())
+        let pastTime = Calendar.current.dateComponents([.year , .month, .day], from: date)
+        
+        print(currentTime.year!)
+        print(pastTime.year!)
+        print(currentTime.month!)
+        print(pastTime.month!)
+        print(currentTime.day!)
+        print(pastTime.day!)
+        if ( currentTime.year! > pastTime.year! || currentTime.month! > pastTime.month! ||  currentTime.day! > pastTime.day!){
+            
+            return true
+        }
+        else { return false }
+        
+    }
+    
+}
+
+
+
+//MARK: - CheckVC UI설정
+extension CheckVC {
+    
+    //MARK:  CheckVCTitle 설정
     func setTitle(){
-        print("CheckVC - setTitle called()")
+        //        print("CheckVC - setTitle called()")
         checkVCTitle.text = initCheckVCTitle // 텍스트 할당3
         // 라벨의 사이즈를 해당크기에 맞게 설정
         checkVCTitle.sizeThatFits(CGSize(width: checkVCTitle.frame.width, height: checkVCTitle.frame.height))
         // checkVCTitle.sizeToFit() -> 자동으로 라벨의 크기를 텍스트에 맞게 수정
+        // 뷰에 오토레이아웃을 작용하기위해 / 뷰에따라 자동으로 제약을 변환하는 기능을 꺼야됨
         
         //autolayout설정으로 인한, 텍스트잘림현상 해결
         checkVCTitle.adjustsFontSizeToFitWidth = true // 라벨의 크기에 맞게 텍스트폰트변경
         checkVCTitle.minimumScaleFactor = 0.2 // 텍스트 간 최소간격
         checkVCTitle.numberOfLines = 1 // 텍스트라인의 수
     }
-    //MARK : - UIButton 설정
     
-    //MARK - RealmData 처리
-    
-    //MARK:  cell에 해당하는 realm데이터 받아옴
-    func getRealmData() {
-        print(" CheckVC setRealmData called() ")
-        let realm = try! Realm()
-        guard let data = realm.objects(Habits.self).filter(NSPredicate(format: "title = %@", initCheckVCTitle )).first else { return }// title을 가지고있는 realm의 index를 찾는다.
-        let time = data.createTime  // 옵셔널 바인딩
-        let count = data.dayCount //
-        pastTime = time // 과거시간
-        dayCount = count // 성공횟수
-        print(data)
-        print(pastTime)
-        print(dayCount)
+    //MARK:  UIButton 설정
+    func setButton(){
+        checkOne.setTitle("", for: .normal)
+        checkTwo.setTitle("", for: .normal)
+        checkThree.setTitle("", for: .normal)
+        
+        successButton.addTarget(self, action: #selector(changeColorSuccessBtn), for: .touchDown)
+    }
+    //MARK: - 이전에 달성한 습관 횟수 버튼에 구현
+    func setButtonImage(_ count : Int){
+        for count in 0..<count{
+            changeButtonImage(count)
+        }
     }
     
-    
-    
-    
+    //MARK: 버튼이 눌리는동안 색 변환
+    @objc func changeColorSuccessBtn(){
+        successButton.backgroundColor = .link
+        successButton.layer.cornerRadius = 5
+        successButton.setTitleColor(.white, for: .normal)
+    }
+    //MARK: 다시 초기 버튼모양
+    func resetSuccessButton(){
+        successButton.setTitle("성공", for: .normal)
+        successButton.setTitleColor(.link, for: .normal)
+        successButton.backgroundColor = .clear
+    }
 }
--- Habits/ConfigVC.swift
+++ Habits/ConfigVC.swift
@@ -120,19 +120,7 @@ extension ConfigureVC : UITableViewDataSource, UITableViewDelegate, RequestLoadL
         loadHabitList()
     }
     
-    // MARK: DateFormatter: 아직 사용 안함
-//    func DateType2String() -> String{
-//        let current = Date()
-//
-//        let formatter = DateFormatter()
-//        //한국 시간으로 표시
-//        formatter.locale = Locale(identifier: "ko_kr")
-//        formatter.timeZone = TimeZone(abbreviation: "KST")
-//        //형태 변환
-//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//
-//        return formatter.string(from: current)
-//    }
+ 
 }
 
 // MARK: - UISearchBarDelegate
