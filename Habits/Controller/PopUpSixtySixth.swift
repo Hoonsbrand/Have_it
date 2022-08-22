dba6b984a479ca7d2f4a4c72f4d328dd4f971ea8
JIHOON4680 <jihoon4680@gmail.com>
2022-08-22 23:28:58 +0900

    CheckVC autolayout, font, size 최종정리 , completeAlert 스타일변경

---
+++ Habits/Controller/PopUpSixtySixth.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpSixtySixth.swift
+//  Habits
+//
+//  Created by 안지훈 on 8/22/22.
+//
+
+import UIKit
+
+class PopUpSixtySixth: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
