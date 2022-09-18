f97e657212b9faf38a1de8d0d770457676d51256
Hoonsbrand <63503972+Hoonsbrand@users.noreply.github.com>
2022-09-18 21:01:42 +0900

    튜토리얼 뷰 컬렉션 뷰 작업

--- Have_it/View/TutorialViewController.swift
+++ Have_it/View/TutorialViewController.swift
@@ -14,6 +14,7 @@ class TutorialViewController: UIViewController {
     @IBOutlet weak var haveItTopAnchor: NSLayoutConstraint!
     @IBOutlet weak var haveItLabel: UILabel!
     @IBOutlet weak var haveItLabelTopAnchor: NSLayoutConstraint!
+    @IBOutlet weak var exampleCollectionView: UICollectionView!
     
     private lazy var exampleLabel: UILabel = {
         let label = UILabel()
